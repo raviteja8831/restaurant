ecfda9b55675889a8b707c48a12638408f225209
naveenkumarreddy150 <you@gmailnaveenkumarreddy1506@gmail.com>
2025-09-16 12:05:02 +0530

    localhost-ip

--- client/app/api.js
+++ client/app/api.js
@@ -10,18 +10,6 @@
 import axios from "axios";
 
 // Detect environment dynamically
-const getBaseURL = () => {
-  if (Platform.OS === "android") {
-    // For Android emulator use 10.0.2.2
-    return "http://10.0.2.2:8080/api";
-  } else if (Platform.OS === "ios") {
-    // For iOS simulator use localhost
-    return "http://localhost:8080/api";
-  } else {
-    // For web/laptop browser
-    return "http://localhost:8080/api";
-  }
-};
 
 const api = axios.create({
   baseURL: getBaseURL(),
