2beeae5e72958a627bc4c4982b6dd421bd5bfad1
Raviteja <raviteja8831@gmail.com>
2025-09-28 08:14:44 +0530

    db changee

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "1509",
-  DB: "res3",
+  HOST: "database-2.cjsqyckyc8sg.ap-south-1.rds.amazonaws.com",
+  USER: "admin",
+  PASSWORD: "oGO1pAtKTehM3rIyWndQ",
+  DB: "restaurant_service",
   dialect: "mysql",
+  timezone: "+05:30", // sets the timezone to IST
+  dialectOptions: {
+    useUTC: false,  // tell the MySQL driver not to convert dates to UTC
+  },
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000,
-  },
+    idle: 10000
+  }
 };
