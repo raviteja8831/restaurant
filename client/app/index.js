6ec8bbf529b9760e917b26750e5fc2a1c59149e5
Raviteja <raviteja8831@gmail.com>
2025-09-29 09:16:10 +0530

    login screen added in index

--- client/app/index.js
+++ client/app/index.js
@@ -1,25 +1,7 @@
 
-import { useEffect } from 'react';
-import { useRouter } from 'expo-router';
-import Constants from 'expo-constants';
-import { DEFAULT_LANDING_PAGE } from './landingConfig';
+
+import LoginScreen from './screens/LoginScreen';
 
 export default function IndexScreen() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Use env/config for landing page, fallback to /login
-    const envLanding = Constants.expoConfig?.extra?.DEFAULT_LANDING_PAGE;
-    const landing = envLanding || DEFAULT_LANDING_PAGE;
-    // Debug log for landing page
-    console.log('Redirecting to landing:', landing);
-    // Delay navigation to ensure router/layout is mounted
-    const timeout = setTimeout(() => {
-      console.log('Router replace called with:', landing);
-      router.replace(landing);
-    }, 200);
-    return () => clearTimeout(timeout);
-  }, [router]);
-
-  return null; // Don't render anything since we're redirecting
+  return <LoginScreen />;
 }
